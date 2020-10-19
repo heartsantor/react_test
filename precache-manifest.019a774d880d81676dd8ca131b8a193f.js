21e54a9323cf887b18e38e3fd1a77783bcfa2de2
heartsantor <antorboss.bd@gmail.com>
2020-10-19 22:48:08 +0600

    Updates

---
+++ precache-manifest.019a774d880d81676dd8ca131b8a193f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c97421e46bc814018b6f5d970abd564",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "da5772ecf268546f667d",
+    "url": "/react_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca893c315cda3b4aa151",
+    "url": "/react_test/static/js/2.ec9c3574.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react_test/static/js/2.ec9c3574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5772ecf268546f667d",
+    "url": "/react_test/static/js/main.7c53e6f3.chunk.js"
+  },
+  {
+    "revision": "0acfb237109b04b55a98",
+    "url": "/react_test/static/js/runtime-main.8b15b1c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
